bc3ca0f40a615768b440a195900cee610eb98cfe
amitmaurya987 <amitkumarmaurya987@gmail.com>
2025-11-10 21:43:05 +0530

    chnagews

--- src/componants/Header.tsx
+++ src/componants/Header.tsx
@@ -49,13 +49,13 @@ const Header = () => {
                 {item.name}
               </a>
             ))}
-            <button
+            {/* <button
               onClick={toggleTheme}
               className="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-800 transition-colors"
               aria-label="Toggle theme"
             >
               {theme === 'light' ? <Moon size={20} /> : <Sun size={20} />}
-            </button>
+            </button> */}
           </div>
 
           <div className="md:hidden flex items-center space-x-4">
